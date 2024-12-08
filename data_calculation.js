f6b9a0f14472cf399c8122c3a033e96f18054081
steve-ly <stevn.ly12@gmail.com>
2024-12-09 10:58:14 +1100

    Adds data_calculation files

---
+++ data_calculation.js
@@ -0,0 +1,19 @@
+
+function calculateValues() {
+    //parse data
+    const json_object = require('./data.json');
+    const req_data = json_object["data"]
+    //initialize values
+    let total_revenue = 0
+    let total_expenses = 0
+    let gross_profit = 0
+    let net_profit = 0
+    let working_capital = 0
+
+    //loop through data object
+    req_data.forEach(item => {
+        //
+    }); 
+}
+
+calculateValues()
